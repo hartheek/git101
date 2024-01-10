e44a45ceed3cf0db5f302bc8c75d3cc9314102f8
hartheek <hartheekreddy@gmail.com>
2024-01-09 23:07:44 -0600

    changes1

--- learn/learn.js
+++ learn/learn.js
@@ -12,3 +12,15 @@ console.log("hellooooooo");
 console.log("hellooooooo");
 console.log("hellooooooo");
 console.log("hellooooooo");
+
+// callback
+
+let a= (b)=>{
+    b();
+    console.log("hello one");
+    b();
+}
+let b= ()=>{
+    console.log("hello two");
+}
+a(b);
